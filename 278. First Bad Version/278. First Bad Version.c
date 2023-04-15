3fd869c6172e4d5512420ea54219fb2a396aa501
mouadbouissel <mouadbouissel@gmail.com>
2023-04-15 16:42:49 +0000

    First Bad Version

---
+++ 278. First Bad Version/278. First Bad Version.c
@@ -0,0 +1,20 @@
+int firstBadVersion(int n) {
+    int left = 1;
+    int right = n;
+    int mid;
+
+    while (left < right) 
+    {
+        mid = left + (right - left) / 2;
+        if (isBadVersion(mid)) 
+        {
+            right = mid;
+        } 
+        else 
+        {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+}
